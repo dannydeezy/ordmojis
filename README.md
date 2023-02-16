850a53e747b3f2fbc1ca21ed694c336b83eccdd8
Danny Diekroeger <dannydiek@gmail.com>
2023-02-15 20:19:34 -0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 Tracking the first of each official [Unicode Emoji](https://unicode.org/emoji/charts/full-emoji-list.html) to be inscribed on bitcoin.
 
-The raw index file is updated regularly at [this link](https://dannydeezy.github.io/ordmojis/lowest-emojis.json).
+The raw index file is updated regularly at [this link](https://dannydeezy.github.io/ordmojis/indexer/lowest-emojis.json).
 
 The valid emoji is the first inscribed `.txt` file containing only the raw emoji Unicode character.
 
--- indexer/index-emojis.js
+++ indexer/index-emojis.js
@@ -34,7 +34,7 @@ function updateReadme() {
     let Readme = `
 Tracking the first of each official [Unicode Emoji](https://unicode.org/emoji/charts/full-emoji-list.html) to be inscribed on bitcoin.
 
-The raw index file is updated regularly at [this link](https://dannydeezy.github.io/ordmojis/lowest-emojis.json).
+The raw index file is updated regularly at [this link](https://dannydeezy.github.io/ordmojis/indexer/lowest-emojis.json).
 
 The valid emoji is the first inscribed \`.txt\` file containing only the raw emoji Unicode character.
 
